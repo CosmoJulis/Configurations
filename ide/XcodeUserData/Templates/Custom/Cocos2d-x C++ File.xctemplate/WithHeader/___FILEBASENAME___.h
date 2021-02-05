08a61c9f8759e75e2e35891749bd7031f66f8c6a
Cosmo Julis <cosmo.julis@gmail.com>
2021-02-05 10:06:16 +0800

    update

---
+++ ide/XcodeUserData/Templates/Custom/Cocos2d-x C++ File.xctemplate/WithHeader/___FILEBASENAME___.h
@@ -0,0 +1,13 @@
+//___FILEHEADER___
+
+#ifndef ___FILEBASENAMEASIDENTIFIER____h
+#define ___FILEBASENAMEASIDENTIFIER____h
+
+#include "cocos2d.h"
+
+class ___FILEBASENAMEASIDENTIFIER___ : public cocos2d::<#node#> {
+public:
+    <#input#>
+};
+
+#endif /* ___FILEBASENAMEASIDENTIFIER____hpp */
